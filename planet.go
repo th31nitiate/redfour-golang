2830569cf36586fc0b3c4ad14db62ac7d4920e2c
m3rl1n th31nitiate <m3rl1n@th31nitiate.xyz>
2023-02-18 22:34:58 +0000

    Added a few more tests & refactor converter funcation

--- planet.go
+++ planet.go
@@ -1,6 +1,6 @@
 package planet
 
-import ("math"; "fmt")
+import ("math")
 
 type Planet struct {
     name string
@@ -11,7 +11,7 @@ type Planet struct {
 type Units struct {
     unit string
     value float64
-    precision int
+    precision uint32
 }
 
 var ngc float64 = 6.67e-11
@@ -24,13 +24,18 @@ func (planet *Planet) calcEV() float64 {
 
 func (units *Units) converter() float64 {
     switch units.unit {
-        case "meters":
-            roundTo(units.value * 5.36819e-6, units.precision)
-            return
-        case "miles":
+        case "to_meters":
+            return units.value * 1000
+        case "to_km":
+            return units.value / 1000
+        case "miles_to_ls":
+            return roundTo(units.value * 5.36819e-6, units.precision)
+        case "inches_to_ls":
+            return roundTo(units.value * 8.472522095734715723e-11, units.precision)
+        case "feet_to_ls":
+            return roundTo(units.value * 1.016702651488166404e-9, units.precision)
+        case "meters_to_ls":
             return roundTo(units.value * 3.335638620368e-9, units.precision)
-        case "kilometers":
-            return roundTo(units.value / 1000, units.precision)
         default:
             return 0.0
     }
--- planet_test.go
+++ planet_test.go
@@ -3,15 +3,29 @@ package planet
 import "testing"
 
 func TestEV(t *testing.T) {
-    t.Log("Running escape velocity tests for earth")
+    t.Log("Running escape velocity tests for Earth")
     earth := Planet{"earth", 6.371e6, 5.97e24}
     if v := earth.calcEV(); v != 11.2 {
         t.Errorf("Issue with escape velocity please check equation: '%f'", v)
     }
 
-    t.Log("Running escape velocity tests for planet X")
-    earth := Planet{"nibiru", 4.0e22, 6.21e6}
-    if v := earth.calcEV(); v != 0.9 {
+    t.Log("Running escape velocity tests for Planet X")
+    nibiru := Planet{"nibiru", 4.0e22, 6.21e6}
+    if v := nibiru.calcEV(); v != 0.9 {
+        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
+    }
+
+    t.Log("Running escape velocity tests for Mars")
+    mars := Planet{"nibiru", 4.0e22, 6.21e6}
+    if v := mars.calcEV(); v != 5.0 {
+        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
+    }
+
+
+    t.Log("Running escape velocity tests for Moon")
+    moon := Planet{"nibiru", 4.0e22, 6.21e6}
+    if v := moon.calcEV(); v != 0.8 {
         t.Errorf("Issue with escape velocity please check equation: '%f'", v)
     }
 }
+
