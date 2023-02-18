f338675c7614d2de8454b9a15d984177a16925e9
m3rl1n th31nitiate <m3rl1n@th31nitiate.xyz>
2023-02-18 20:01:03 +0000

    Golang planet test with basic calc ev

--- planet.go
+++ planet.go
@@ -1,6 +1,6 @@
 package planet
 
-import ("math")
+import ("math";)
 
 
 type Planet struct {
@@ -11,13 +11,13 @@ type Planet struct {
 
 var ngc float64 = 6.67e-11
 
-func calcEV() float64 {
-    earth := Planet{"earth", 6.371e6, 5.97e24}
-    gmr := 2 * ngc * earth.mass / earth.radius
+func (planet *Planet) calcEV() float64 {
+    gmr := 2 * ngc * planet.mass / planet.radius
     vms := math.Sqrt(gmr)
     vkms := vms / 1000
     return roundTo(vkms, 1)
 }
+
 
 func roundTo(n float64, decimals uint32) float64 {
     return math.Round(n * math.Pow(10, float64(decimals))) / math.Pow(10, float64(decimals))
--- planet_test.go
+++ planet_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestEV(t *testing.T) {
     t.Log("Running Escape velocity tests")
-    if v := calcEV(); v != 11.2 {
+    earth := Planet{"earth", 6.371e6, 5.97e24}
+    if v := earth.calcEV(); v != 11.2 {
         t.Errorf("Issue with escape velocity please check equation: '%f'", v)
     }
 
