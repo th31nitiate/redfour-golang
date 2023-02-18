f9fa08bb226766124f2fa483e0a12552bba50c3a
m3rl1n th31nitiate <m3rl1n@th31nitiate.xyz>
2023-02-18 22:02:12 +0000

    Refactored funcation and added EV test for planetX

--- planet.go
+++ planet.go
@@ -1,7 +1,6 @@
 package planet
 
-import ("math";)
-
+import ("math"; "fmt")
 
 type Planet struct {
     name string
@@ -9,15 +8,34 @@ type Planet struct {
     mass float64
 }
 
+type Units struct {
+    unit string
+    value float64
+    precision int
+}
+
 var ngc float64 = 6.67e-11
 
 func (planet *Planet) calcEV() float64 {
     gmr := 2 * ngc * planet.mass / planet.radius
     vms := math.Sqrt(gmr)
-    vkms := vms / 1000
-    return roundTo(vkms, 1)
+    return vms
 }
 
+func (units *Units) converter() float64 {
+    switch units.unit {
+        case "meters":
+            roundTo(units.value * 5.36819e-6, units.precision)
+            return
+        case "miles":
+            return roundTo(units.value * 3.335638620368e-9, units.precision)
+        case "kilometers":
+            return roundTo(units.value / 1000, units.precision)
+        default:
+            return 0.0
+    }
+    return 0.0
+}
 
 func roundTo(n float64, decimals uint32) float64 {
     return math.Round(n * math.Pow(10, float64(decimals))) / math.Pow(10, float64(decimals))
--- planet_test.go
+++ planet_test.go
@@ -3,10 +3,15 @@ package planet
 import "testing"
 
 func TestEV(t *testing.T) {
-    t.Log("Running Escape velocity tests")
+    t.Log("Running escape velocity tests for earth")
     earth := Planet{"earth", 6.371e6, 5.97e24}
     if v := earth.calcEV(); v != 11.2 {
         t.Errorf("Issue with escape velocity please check equation: '%f'", v)
     }
 
+    t.Log("Running escape velocity tests for planet X")
+    earth := Planet{"nibiru", 4.0e22, 6.21e6}
+    if v := earth.calcEV(); v != 0.9 {
+        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
+    }
 }
