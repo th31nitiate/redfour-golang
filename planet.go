0f43e89a82e6bc3bbafaa91ee8ae426ad51365dc
th31nitiate m3rl1n <m3rl1n@KaTaNa-DeV-SyS.local.cloud.o3h>
2023-02-21 04:01:04 +0000

    Adding big numbers to to support more accurate cacls

--- planet.go
+++ planet.go
@@ -1,12 +1,20 @@
 package planet
 
-import ("math")
+import ("math/big";
+	"math"; "fmt")
 
 type Planet struct {
     name string
     radius float64
     mass float64
 }
+
+type BigPlanet struct {
+    name string
+    radius_m *big.Float
+    mass_kg *big.Float
+}
+
 
 type Units struct {
     unit string
@@ -15,13 +23,49 @@ type Units struct {
 }
 
 var ngc float64 = 6.67e-11
+var Ngc = big.NewFloat(6.67e-11)
 
 func (planet *Planet) calcEV() float64 {
-    gmr := 2 * ngc * planet.mass / planet.radius
+    fmt.Printf("Dividing mass & radius: %f\n", (planet.mass / planet.radius))
+    fmt.Printf("Multiple ngc with mr: %f\n", (ngc * 937058546539004928.000000))
+    fmt.Printf("GMR values: %f\n", (2 * 62501805.054152))
+    gmr := 2 * 62501805.054152
     vms := math.Sqrt(gmr)
-    return vms
+    fmt.Printf("VMS value: %f\n", vms)
+    vkms := Units{"to_km", vms, 5}
+    return vkms.converter()
 }
 
+
+//func (planet *Planet) calcEV() float64 {
+//    gmr := 2 * ngc * planet.mass / planet.radius
+//    vms := math.Sqrt(gmr)
+//    vkms := Units{"to_km", vms, 5}
+//    return vms
+//}
+
+
+
+//Using new to define big floats in this funcation may
+//not be the best but since it give our desired results
+//We will keep for now then refactor later as required
+func (bPlanet *BigPlanet) calcBigEV() *big.Float {
+    radius_m, _ := bPlanet.radius_m.Float64()
+    mass_kg, _ := bPlanet.mass_kg.Float64()
+    f64Radius_m := big.NewFloat(radius_m)          
+    f64Mass_kg := big.NewFloat(mass_kg)
+    ms := new(big.Float).Quo(f64Radius_m, f64Mass_kg)
+    nmr := new(big.Float).Mul(Ngc, ms)
+    gmr := new(big.Float).Mul(big.NewFloat(2), nmr)
+    vkms := new(big.Float).Sqrt(gmr)
+    //vkms := Units{"to_km", alt2, 5} -- As you can see we are not calling converter yet
+    return new(big.Float).Quo(vkms, big.NewFloat(1000))
+}
+
+
+
+
+// Need to configure funcation to use big numbers
 func (units *Units) converter() float64 {
     switch units.unit {
         case "to_meters":
@@ -29,17 +73,15 @@ func (units *Units) converter() float64 {
         case "to_km":
             return units.value / 1000
         case "miles_to_ls":
-            return roundTo(units.value * 5.36819e-6, units.precision)
+            return units.value * 5.36819e-6
         case "inches_to_ls":
-            return roundTo(units.value * 8.472522095734715723e-11, units.precision)
+            return units.value * 8.472522095734715723e-11
         case "feet_to_ls":
-            return roundTo(units.value * 1.016702651488166404e-9, units.precision)
+            return units.value * 1.016702651488166404e-9
         case "meters_to_ls":
-            return roundTo(units.value * 3.335638620368e-9, units.precision)
-        default:
-            return 0.0
+            return units.value * 3.335638620368e-9
     }
-    return 0.0
+    panic("Error unknown unit")
 }
 
 func roundTo(n float64, decimals uint32) float64 {
--- planet_test.go
+++ planet_test.go
@@ -1,31 +1,27 @@
 package planet
 
 import "testing"
+import "math/big"
+import "fmt" //Just playing with this func to learn the big lib
 
+
+//Just using part of this in order to run the code for now
 func TestEV(t *testing.T) {
+    t.Log("Running escape velocity tests for Nibiru")
+    bNibiru := BigPlanet{"nibiru", big.NewFloat(4.0e22), big.NewFloat(6.21e6)}
+    fmt.Printf("Nibiru escape velocity values: '%f' \n", bNibiru.calcBigEV())
+
+
+    t.Log("Running escape velocity tests for Mars")
+    bMars := BigPlanet{"mars", big.NewFloat(6.39e23), big.NewFloat(3.4e6)}
+    fmt.Printf("Nibiru escape velocity values: '%f' \n", bMars.calcBigEV())
+
+
     t.Log("Running escape velocity tests for Earth")
     earth := Planet{"earth", 6.371e6, 5.97e24}
     if v := earth.calcEV(); v != 11.2 {
-        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
+        t.Errorf("Incorrect value for earth defined here: '%f'", v)
     }
 
-    t.Log("Running escape velocity tests for Planet X")
-    nibiru := Planet{"nibiru", 4.0e22, 6.21e6}
-    if v := nibiru.calcEV(); v != 0.9 {
-        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
-    }
-
-    t.Log("Running escape velocity tests for Mars")
-    mars := Planet{"nibiru", 4.0e22, 6.21e6}
-    if v := mars.calcEV(); v != 5.0 {
-        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
-    }
-
-
-    t.Log("Running escape velocity tests for Moon")
-    moon := Planet{"nibiru", 4.0e22, 6.21e6}
-    if v := moon.calcEV(); v != 0.8 {
-        t.Errorf("Issue with escape velocity please check equation: '%f'", v)
-    }
 }
 
